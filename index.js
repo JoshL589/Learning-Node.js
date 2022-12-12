a1f4886da2709b045d427fc138119c8eeb3ed145
Joshua Li <joshuazli2020@gmail.com>
2022-12-12 13:44:22 +1000

    adding cors to access the server from my react app

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+
+app.use(cors());
+
 let notes = [
   {
     id: 1,
@@ -74,7 +78,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
